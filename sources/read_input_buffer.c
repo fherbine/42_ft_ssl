7a290a57d4507e44505296b836d0fd72179b0f22
fherbine <felix.herbinet@yahoo.com>
2020-11-02 19:04:57 +0100

    Fix to avoid mem leaks

--- sources/read_input_buffer.c
+++ sources/read_input_buffer.c
@@ -17,6 +17,7 @@ static char	*catbuffer(char *s1, char *s2)
 	char	*dst;
 
 	dst = ft_strcat(s1, s2);
+	free(s1);
 	return (dst);
 }
 
@@ -28,12 +29,15 @@ static char	*read_buffer(int fd)
 
 	if (!(buf = (char *)malloc(sizeof(char) * (BUFFER + 1))))
 		return (NULL);
-	result = "";
+	if (!(result = (char *)malloc(sizeof(char) * 1)))
+		return (NULL);
+	result[0] = '\0';
 	while ((bz = read(fd, buf, BUFFER)) > 0)
 	{
 		buf[bz] = '\0';
 		result = catbuffer(result, buf);
 	}
+	free(buf);
 	return (result);
 }
 
